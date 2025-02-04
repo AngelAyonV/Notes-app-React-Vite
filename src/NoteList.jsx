3416d40497870f8511d206acebccf69f89596058
AngelAyonV <angelayon815@gmail.com>
2025-02-04 14:42:47 -0700

    agregando dates unicas

--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -5,9 +5,14 @@ const NoteList = ({ note, removeCompletedNote }) => {
   // Actualiza el estado solo cuando 'note' cambie
   useEffect(() => {
     if (Array.isArray(note)) {
-      setlistNotes(note);
+      // Transforma cada nota en un objeto con texto y fecha única
+      const updatedNotes = note.map((text) => ({
+        text,
+        date: new Date().toISOString(), // Fecha única para cada nota
+      }));
+      setlistNotes(updatedNotes);
     } else {
-      setlistNotes([]); // Previene errores si task no es un array
+      setlistNotes([]); // Previene errores si note no es un array
     }
   }, [note]);
 
@@ -19,11 +24,12 @@ const NoteList = ({ note, removeCompletedNote }) => {
   };
   return (
     <ol>
-      {listNotes?.map((t, index) => (
+      {listNotes?.map((note, index) => (
         <div key={index} style={{ display: "flex", alignItems: "center" }}>
-          <li>{t}</li>
+          <li>{note.text}</li>
+          {/* Mostrar la fecha */}
           <span style={{ margin: "0 10px" }}>
-            {new Date().toLocaleString()} {/* Fecha y hora actual */}
+            {new Date(note.date).toLocaleString()} {/* Formatea la fecha */}
           </span>
           <button
             className="delete-note-btn"
